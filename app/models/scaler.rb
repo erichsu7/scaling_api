6b8118bf8782b94b1f218698c0de1a450963c135
Eric Hsu <erichsu7@gmail.com>
2015-04-29 11:22:11 -0700

    Add validation for image_dimensions and bounding_box

--- app/models/scaler.rb
+++ app/models/scaler.rb
@@ -1,8 +1,25 @@
 class Scaler < ActiveRecord::Base
   attr_accessor :image_dimensions, :bounding_box
+  validate :even_number_of_image_dimensions, :two_bounding_box_dimensions
 
   def initialize(image_dimensions, bounding_box)
     @image_dimensions = image_dimensions
-    @bounding_bo = bounding_box
+    @bounding_box = bounding_box
+  end
+
+  def parse_image_dimensions
+    image_dimensions = image_dimensions[1..-2].split(",").map {|dim| dim.to_i}
+  end
+
+  def parse_bounding_box
+    bounding_box = bounding_box[1..-2].split(",").map {|dim| dim.to_i}
+  end
+
+  def even_number_of_image_dimensions
+    image_dimensions.length % 2 == 0
+  end
+
+  def two_bounding_box_dimensions
+    bounding_box.length == 2
   end
 end
